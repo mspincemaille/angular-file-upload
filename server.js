0fc6ab4edf2c4650de1365bc5a4aa9acc4fd067a
Miguel Spincemaille <miguelspincemaille@hotmail.com>
2023-09-28 09:15:24 +0200

    (chore): removed unused express server folder

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+
+// library for reading multipart data eg files
 const efu = require('express-fileupload');
 
 const app = express();
-const LOCAL_PORT = 3000;
+const LOCAL_PORT = process.env.LOCAL_PORT;
 
+// mongodb setup
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL + 'files', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error('database error:', error));
+db.once('open', () => { console.log('Connected to database')});
+
+// server injections
 app.use(cors());
 app.use(efu());
 
