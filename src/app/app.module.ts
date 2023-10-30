2a70fdad76291f6fc47b7b2f5c6cc32ae2a14ccf
Miguel Spincemaille <miguelspincemaille@hotmail.com>
2023-10-30 22:40:10 +0100

    (chore): change theme + mat-selection-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
